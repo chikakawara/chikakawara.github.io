aa5d4f0be8b7d6399724bdd44c3b4454869b0378
Chika Kawara <shikinami@gmail.com>
2018-04-29 16:48:15 +0900

    add jQuery and to_pagetop btn

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  var to_pagetop = $('.to_pagetop');
+    $(window).scroll(function () {
+       if ($(this).scrollTop() > 100) {
+            to_pagetop.fadeIn();
+       } else {
+            to_pagetop.fadeOut();
+            }
+       });
+       to_pagetop.click(function () {
+           $('body, html').animate({ scrollTop: 0 }, 500);
+              return false;
+   });
+});
